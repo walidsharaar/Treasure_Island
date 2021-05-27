696af8df1dfe64312a9741ec8970fb072c80baf6
Sharaar <walidsharar@gmail.com>
2021-05-27 18:03:48 +0200

    Intial code is created

---
+++ main.py
@@ -0,0 +1,52 @@
+
+print('''*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+#link of the flowchart is given below
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+user_choice = input("You are at a crossroad! do you wanna go Right or Left?\n").lower()
+
+if user_choice == "right":
+    print("You fall into a hole. Game Over!!!")
+elif user_choice == "left":
+    swim_ability = input("Can you swim? Yes or No ").lower()
+    if swim_ability == "no":
+        print("Attacked by trout. Game Over!!!")
+    elif swim_ability == "yes":
+        door_selection = input("Which door color are going to select? ").lower()
+        if door_selection == "red":
+            print("Burned by fire. Game Over!!!")
+        elif door_selection == "blue":
+            print("Eaten by beast. Game Over!!!")
+        elif door_selection =="yellow":
+            print("You win!!!")
+        else:
+            print("Game Over!!!")
+else:
+    print(" Game Over!!!")
+
+
+
+
